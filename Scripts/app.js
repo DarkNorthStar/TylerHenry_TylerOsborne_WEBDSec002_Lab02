51f3d444cee8ccfc4ef0044af337c39882532dab
DarkNorthStar <mr_tyler09@live.ca>
2022-02-26 15:49:18 -0500

    Moved User.js into app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -334,6 +334,50 @@ function updateTime()
     }
                   
 } 
+// Class for users
+class User {
+    // Constructor
+    constructor(firstName,lastName,username,email,password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    // Getters
+    get getFirstName(){
+        return this.firstName;
+    }
+    get getLastName(){
+        return this.lastName;
+    }
+    get getUsername(){
+        return this.username;
+    }
+    get getEmail(){
+        return this.email;
+    }
+    get getPassword(){
+        return this.password;
+    }
+    // Setters
+    set setFirstName(firstName){
+        this.firstName = firstName;
+    }
+    set setLastName(lastName){
+        this.lastName = lastName;
+    }
+    set setUsername(username){
+        this.username = username;
+    }
+    set setEmail(email){
+        this.email = email;
+    }
+    set setPassword(password){
+        this.password = password;
+    }
+
+  }
 
 // On launch check which page is the current page
 window.addEventListener( "load", checkPage, false );
